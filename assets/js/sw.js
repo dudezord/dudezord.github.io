fabd0b93bb9192208baabd43e62c783128443a9e
dudezord <dudezord@protonmail.com>
2022-04-09 14:43:32 +0100

    Add 2022-04-09-making-my-first-game-portcullis-part-2

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -29,7 +29,7 @@ const SHELL_CACHE = "shell-9.1.6--v13--sw/";
 const ASSETS_CACHE = "assets--v13--sw/";
 
 // The cache for regular content, which will be invalidated every time you make a new build.
-const CONTENT_CACHE = "content--2022-03-31T01:09:59+01:00--sw/";
+const CONTENT_CACHE = "content--2022-04-09T14:42:35+01:00--sw/";
 
 // A URL search parameter you can add to external assets to cache them in the service worker.
 const SW_CACHE_SEARCH_PARAM = "sw-cache";
@@ -169,6 +169,11 @@ const STATIC_FILES = [
   /**/"/assets/img/blog/jj-ying.jpg",
   /**/"/assets/img/blog/louis-hansel.jpg",
   /**/"/assets/img/blog/posts/01-portcullis/part-1.jpg",
+  /**/"/assets/img/blog/posts/01-portcullis/part-2-Game_Loop.webm",
+  /**/"/assets/img/blog/posts/01-portcullis/part-2-Gate_Animation.webm",
+  /**/"/assets/img/blog/posts/01-portcullis/part-2-gate-placeholder.jpg",
+  /**/"/assets/img/blog/posts/01-portcullis/part-2-mob-placeholder.jpg",
+  /**/"/assets/img/blog/posts/01-portcullis/part-2.jpg",
   /**/"/assets/img/blog/posts/press-start.jpg",
   /**/"/assets/img/blog/posts/solo-developers.png",
   /**/"/assets/img/blog/steve-harvey.jpg",
